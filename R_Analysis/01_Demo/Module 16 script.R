12477e8b5b90b4185cec9bf2d4f2bd222bc3f2d3
Marco <marco_mireles@outlook.com>
2023-01-05 18:19:51 -0600

    16.3.2 create a summary table

--- R_Analysis/01_Demo/Module 16 script.R
+++ R_Analysis/01_Demo/Module 16 script.R
@@ -6,4 +6,14 @@ wide_table <- long_table %>% spread(key="Metric",value="Score")
 
 # 16.3.1 Introduction to ggplot2
 ?ggplot()
-  
+head(mpg)  
+
+# 16.3.2 Build a bar plot in ggplot 2
+# Import data set into ggplot 2
+plt <- ggplot(mpg,aes(x=class))
+# Plot a bar plot
+plt + geom_bar()
+# Bar charts documentation
+?geom_bar()
+# Create a summary table
+mpg_summary <- mpg %>% group_by(manufacturer) %>% summarize(Vehicle_Count=n(), .groups = 'keep')
